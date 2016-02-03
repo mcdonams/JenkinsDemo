f7e3fa2345cab1a1e4c2ff47b40d17d8ecc0ac36
Mike M <mcdonams@hotmail.com>
2016-02-02 22:42:13 -0500

    Initial code commit

---
+++ JenkinsDemo.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JenkinsDemo;
+
+namespace JenkinsDemo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetMessage_Returns_Correct_String()
+        {
+            Assert.AreEqual(Program.GetMessage(), "Hello Jenkins!");
+        }
+    }
+}
---
+++ JenkinsDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMessage());
+        }
+
+        internal static string GetMessage()
+        {
+            return "Hello Jenkins!";
+        }
+    }
+}
